85fbcb1010bf08eea73fa4da7987acf0f3b93c3d
Ilya Ashchepkov <koctep@gmail.com>
2017-07-01 17:57:18 +0000

    Update component template

--- generator/component/temp.js
+++ generator/component/temp.js
@@ -2,9 +2,16 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import <%= name %>Component from './<%= name %>.component';
 
-let <%= name %>Module = angular.module('<%= name %>', [
-  uiRouter
-])
+let <%= name %>Module = angular.module('<%= name %>', [uiRouter])
+
+.config(($stateProvider) => {
+  'ngInject';
+  $stateProvider
+    .state('<%= name %>', {
+      url: '/<%= name %>',
+      template: '<<%= name %>></<%= name %>>'
+    });
+})
 
 .component('<%= name %>', <%= name %>Component);
 
