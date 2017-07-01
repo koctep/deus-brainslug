7f2f3213d15755dd1c34a3949be88daf9b17ed76
Ilya Ashchepkov <koctep@gmail.com>
2017-07-01 20:28:47 +0000

    Clear code

--- client/app/app.js
+++ client/app/app.js
@@ -1,13 +1,14 @@
+import 'angular-material/angular-material.css';
+
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import Navbar from './navbar/navbar';
-import Components from './components/components';
-import AppComponent from './app.component';
-
-import 'angular-material/angular-material.css';
 import angularAnimate from 'angular-animate';
 import angularMaterial from 'angular-material';
 
+import Navbar from './navbar/navbar';
+import Components from './components/components';
+
+import AppComponent from './app.component';
 import AppController from './app.controller';
 
 var app = angular.module('app', [
@@ -18,12 +19,14 @@ var app = angular.module('app', [
   Navbar.name
 ]);
 
-
-app.config(($locationProvider) => {
+app.config(($locationProvider, $qProvider, $urlRouterProvider) => {
   "ngInject";
+
   // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
   // #how-to-configure-your-server-to-work-with-html5mode
   $locationProvider.html5Mode(true).hashPrefix('!');
+  $qProvider.errorOnUnhandledRejections(false);
+  $urlRouterProvider.otherwise('/init');
 });
 
 app.controller(AppController.name, AppController);
